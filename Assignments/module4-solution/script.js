97496d0a06220421bab76780537f9738179ba363
SoloistCoder <antalshubham501@gmail.com>
2021-07-11 21:17:51 +0530

    updated names

--- Assignments/module4-solution/script.js
+++ Assignments/module4-solution/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "Jasmeet", "Jack", "Jai", "Shubham", "Muskan", "Jass", "Anuj", "Gurbaz", "John"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
